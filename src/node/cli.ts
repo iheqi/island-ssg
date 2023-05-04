03dcfe3a54541cdd4cc4bc733d9e0098798a7eac
何琦 <>
2023-05-04 12:56:16 +0800

    4.项目初始化: CLI脚手架/Dev Server

---
+++ src/node/cli.ts
@@ -0,0 +1,26 @@
+const path = require('path');
+
+import { cac } from 'cac';
+import { createDevServer } from './dev';
+
+const version = require("../../package.json").version;
+const cli = cac('island').version(version).help();
+
+cli
+  .command("[root]", "start dev server")
+  .alias('dev')
+  .action(async (root: string) => {
+    root = root ? path.resolve(root) : process.cwd();
+    const server = await createDevServer(root);
+
+    await server.listen();
+    server.printUrls();
+  });
+
+cli
+  .command("build [root]", "build for production")
+  .action(async (root: string) => {
+    console.log("build", root);
+  });
+
+cli.parse();  
